928eb9353e5e74e8ac929ea9d03fb287f61c5e1a
Ilya Danilov <esozap32@mail.ru>
2022-11-05 20:54:38 +0400

    GeometricProgression

--- src/main/java/GeometricProgression.java
+++ src/main/java/GeometricProgression.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class GeometricProgression {
+public class GeometricProgression {
+    public static void main(String[] args) {
+        int Degree = 2;
+        int beginning = 1;
+        int theAmount = beginning;
+
+        do {
+            System.out.println("theAmount = " + theAmount);
+            theAmount = (beginning + (theAmount * Degree));
+
+        } while (theAmount <= 100);
+    }
 }
